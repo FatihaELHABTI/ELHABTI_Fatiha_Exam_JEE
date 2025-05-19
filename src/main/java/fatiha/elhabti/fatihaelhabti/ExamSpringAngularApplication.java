a20b30e613579ddaa91a8ecead099b486b282b72
lilyhabti <105781552+lilyhabti@users.noreply.github.com>
2025-05-19 09:06:04 +0100

    creation du projet et ajout des dependences

---
+++ src/main/java/fatiha/elhabti/fatihaelhabti/ExamSpringAngularApplication.java
@@ -0,0 +1,13 @@
+package fatiha.elhabti.fatihaelhabti;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamSpringAngularApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExamSpringAngularApplication.class, args);
+    }
+
+}
---
+++ src/test/java/fatiha/elhabti/fatihaelhabti/ExamSpringAngularApplicationTests.java
@@ -0,0 +1,13 @@
+package fatiha.elhabti.fatihaelhabti;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamSpringAngularApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
